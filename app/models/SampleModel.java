26997904149609309f44e320c28e0421889a666e
shinyaa31 <shinyaa31@gmail.com>
2013-07-02 20:28:24 +0900

    Playframework実行の為にはパッケージ内にクラスファイルが無いといけないようなので追加

---
+++ app/models/SampleModel.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shinyaa31
+ * Date: 13/07/02
+ * Time: 20:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class SampleModel {
+}
