d57bcdf58aba2be7961114d89b2a73a3a065e876
shinyaa31 <shinyaa31@gmail.com>
2013-07-02 20:42:49 +0900

    パッケージ構成整備等

---
+++ app/groovy/aws/util/AwsCredentials.groovy
@@ -0,0 +1,11 @@
+package groovy.aws.util
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shinyaa31
+ * Date: 13/07/02
+ * Time: 20:33
+ * To change this template use File | Settings | File Templates.
+ */
+class AwsCredentials {
+}
